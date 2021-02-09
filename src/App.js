997348aad48198fdf97945f8e41fdda53f1a1866
NaoKanata <kanatanao7o@gmail.com>
2021-02-09 10:12:55 +0900

    簡易的なオプション画面を作成

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import StopIcon from '@material-ui/icons/Stop';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            anim: 0
+        };
+    }
+    render() {
+        let data = localStorage.getItem("isRunning");
+        return(
+            <div className="App">
+                { data !== "true" ? this._renderPlay() : this._renderStop() }
+            </div>
+        );
+    }
+
+    _renderStop() {
+        return (
+            <Button variant="contained" color="primary" startIcon={<StopIcon />} onClick={this._onClick}>
+                Stop
+            </Button>
+        );
+    }
+
+    _renderPlay() {
+        return (
+            <Button variant="contained" color="primary" startIcon={<PlayArrowIcon />} onClick={this._onClick}>
+                START
+            </Button>
+        );
+    }
+
+    _onClick = () => {
+        this.setState({ anim: 1 });
+        let data = localStorage.getItem("isRunning");
+        localStorage.setItem("isRunning", data === "true" ? "false" : "true");
+    }
 }
 
 export default App;
